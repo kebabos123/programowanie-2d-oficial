7515996c358a3bb5e27485c720b884783f06b987
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-29 13:15:58 +0200

    if

---
+++ programowanie/InstructiomIfConsoleApplication/InstructiomIfConsoleApplication.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+//napisz funkcjê która podzieli 2 liczby
+void task1()
+{
+    float first_number;
+    float second_nuber;
+    cout << "podaj pierwsza liczbe\n";
+    cin >> first_number;
+    cout << "podaj druge liczbe\n";
+    cin >> second_nuber;
+    cout << "wynik dzielenia to\n" << first_number / second_nuber;
+    
+
+}
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task1();
+}
