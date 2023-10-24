41a67f55248c8c47fd47a4319a040b8809e073f6
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-24 15:23:42 +0200

    lekcja

---
+++ programowanie/LoopConsoleApplication/LoopConsoleApplication.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+void task1()
+{
+    int number;
+    cin >> number;
+    while (number != 0)
+    {
+        cin >> number;
+    }
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task1();
+    
+}
