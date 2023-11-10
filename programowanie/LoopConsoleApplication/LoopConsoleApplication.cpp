b261025c471b119e4b87baa6641b3349500041b8
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-10 12:50:04 +0100

    lekcja

--- programowanie/LoopConsoleApplication/LoopConsoleApplication.cpp
+++ programowanie/LoopConsoleApplication/LoopConsoleApplication.cpp
@@ -45,6 +45,82 @@ void task4for()
         factorial = factorial * i;
     }
     cout << "silnia liczby winosi: " << factorial;
+} 
+//Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4!itd.)
+void task5for()
+{
+    int silnia=1;
+    for (int i = 1; i <= 10; i++)
+    {
+
+            silnia = i * silnia;
+            cout << silnia << "\n";
+    }
+}
+//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1 = 1, 1x2 = 2, 1x3 = 3 itd.).
+void task6for()
+{
+    for (int i = 1; i < 10; i++)
+    {
+        for (int k = 1; k < 10; k++)
+        {
+            cout << i * k<<"\n";
+        }
+    }
+}
+//Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+void task7for()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+        {
+            cout << i<<"\n";
+        }
+    }
+}
+// Program obliczający sumę kwadratów liczb od 1 do 10
+void task8for()
+{
+    int k = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        k = (i * i) + k;
+        
+    }
+    cout << k;
+}
+/*Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu
+(ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)*/
+void task9for()
+{
+    int fib1=1, fib2=0;
+    for (int i = 1; i <= 20; i++)
+    {
+        int fib = fib1 + fib2;
+        cout << fib<<"\n";
+        fib2 = fib1;
+        fib1 = fib;
+        
+    }
+}
+/** Program, kóry wyświetli poniższe cztery zwory:
+    ****       54321        121212        122333
+    ***        65432        212121        223334444
+    **         76543        121212        333444455555
+    *          87654        212121        444455555666666*/
+void task10for()
+{
+    
+    for (int i = 4; i > 0; i--)
+    {
+        for (int j = 0; i > j; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+        
+    }
 }
 
 int main()
@@ -54,6 +130,12 @@ int main()
     //task2for();
     //task3for();
     //task4for();
+    //task5for();
+    //task6for();
+    //task7for();
+    //task8for();
+    //task9for();
+    //task10for();
     
 }
 
@@ -74,16 +156,12 @@ FOR
 
 
 
-* Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
-* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
-* Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
-* Program obliczający sumę kwadratów liczb od 1 do 10
-* Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
-* Program, kóry wyświetli poniższe cztery zwory:
-    ****       54321        121212        122333
-    ***        65432        212121        223334444
-    **         76543        121212        333444455555
-    *          87654        212121        444455555666666
+
+
+
+
+
+
 
 * Oblicz sumę szeregu 1/1^2 + 1/2^2 + 1/3^2 + ... +1/n^2. Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i spierwiastkowaniu powinieneś otrzymać wartość liczby π (suma szeregu jest równa π2/6). Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
 
