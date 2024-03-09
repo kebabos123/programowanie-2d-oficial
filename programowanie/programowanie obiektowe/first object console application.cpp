f43c4080e1e82b80326665d5c3c8da26c9ea3d99
kebap <bakalarzkarol5@gmail.com>
2024-03-09 22:10:36 +0100

    dom

--- programowanie/programowanie obiektowe/first object console application.cpp
+++ programowanie/programowanie obiektowe/first object console application.cpp
@@ -2,85 +2,147 @@
 #include <cmath>
 using namespace std;
 
-class Point
+//class Point
+//{
+//private:
+//    int x;
+//    int y;
+//    int z;
+//
+//public:
+//    Point()
+//    {
+//        x = 3;
+//        y = 7;
+//        z = 10;
+//    }
+//
+//    Point(int xx)
+//    {
+//        x = xx;
+//        y = 7;
+//        z = 10;
+//    }
+//
+//    Point(int xx, int yy, int zz)
+//    {
+//        x = xx;
+//        y = yy;
+//        z = zz;
+//    }
+//
+//    void SetX(int value)
+//    {
+//        //x=abs(value)
+//
+//        if (value >= 0)
+//        {
+//            x = value;
+//        }
+//        else
+//        {
+//            //reakcja na b³¹d
+//        }
+//    }
+//    int GetX()
+//    {
+//        //if(uprawnienia)
+//        return x;
+//    }
+//
+//    double distanceFromCenter(Point p)
+//    {
+//        double distance;
+//        distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+//        return distance;
+//    }
+//};
+//class Rect
+//   {
+//       private:
+//        float height;
+//        float width;
+//       public:
+//        rect()
+//        {
+//           height = 5;
+//           width = 3;
+//        }
+//        float circuitOfRect()
+//        {
+//           float circuit = 2 * height + 2 + width;
+//        }
+//        float areaOfRect()
+//        {
+//           float area = width * height;
+//        }
+//        float diagonalOfRect()
+//        {
+//           float diagonal = sqrt(height * height + width * width);
+//        }
+//   };
+class Konto
 {
 private:
-    int x;
-    int y;
-    int z;
-
+    string login;
+    string password;
+    double balance;
 public:
-    Point()
+    Konto()
     {
-        x = 3;
-        y = 7;
-        z = 10;
+        login = "undefined";
+        password = "undefined";
+        balance = 0;
     }
-
-    Point(int xx)
+    void createLogin()
     {
-        x = xx;
-        y = 7;
-        z = 10;
+        cout << "wpisz login"<<'\n';
+        cin >> login;
+        cout << '\n';
     }
-
-    Point(int xx, int yy, int zz)
+    void createPassword()
     {
-        x = xx;
-        y = yy;
-        z = zz;
+        cout << "wpisz hasło"<<'\n';
+        cin >> password;
+        cout << '\n';
     }
-
-    void SetX(int value)
+    void logIn()
     {
-        //x=abs(value)
-
-        if (value >= 0)
+        string l;
+        string p;
+        while (true)
         {
-            x = value;
-        }
-        else
-        {
-            //reakcja na b³¹d
+            if (login != "undefined" && password != "undefined")
+            {
+                cout << "logowanie:\n"<<"wpisz login\n";
+                cin >> l;
+                cout << "wpisz hasło\n";
+                cin >> p;
+                if (l != login || p != password)
+                {
+                    cout << "niepoprawne dane\n";
+                    continue;
+                }
+                else
+                {
+                    cout << "zostałeś zalogowany\n";
+                    break;
+                }
+            }
         }
     }
-    int GetX()
+    void addMoney()
     {
-        //if(uprawnienia)
-        return x;
+        cout << "ile pieniędzy chcesz dodać na konto?";
+        cin >> balance;
+        cout << "dodane!";
+    }
+    void transaction(Konto &targetAccount)
+    {
+        targetAccount.
     }
 
-    double distanceFromCenter(Point p)
-    {
-        double distance;
-        distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
-        return distance;
-    }
 };
-   class rect
-   {
-       private:
-        float height;
-        float width;
-       public:
-        rect()
-        {
-           height = 5;
-           width = 3;
-        }
-        float circuitOfRect()
-        {
-           float circuit = 2 * height + 2 + width;
-        }
-        float areaOfRect()
-        {
-           float area = width * height;
-        }
-        float diagonalOfRect()
-        {
-           float diagonal = sqrt(height * height + width * width);
-        }
-   };
 int main()
 {
     setlocale(LC_CTYPE, "polish");
@@ -89,5 +151,15 @@ int main()
     // firstPoint.x = 3;
     // firstPoint.y = 5;
     //firstPoint.z = 8;
-    rect firstRectangle;     
+    //rect firstRectangle;   
+    Konto firstAccount;
+    firstAccount.createLogin();
+    firstAccount.createPassword();
+    firstAccount.logIn();
+    firstAccount.addMoney();
+    Konto secondAccount;
+    secondAccount.createLogin();
+    secondAccount.createPassword();
+    secondAccount.logIn();
+    secondAccount.addMoney();
 }
